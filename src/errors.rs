89ea99cde865202519b04c87e0682e0ce1b4d984
mmynk <mohit.ritanil@gmail.com>
2023-10-20 12:51:53 -0500

    Split `TcError` into `NetlinkError` and `tc` related error

--- src/errors.rs
+++ src/errors.rs
@@ -1,9 +1,10 @@
 use std::error;
 
+use bincode::ErrorKind;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-pub enum TcError {
+pub enum NetlinkError {
     #[error("Failed to create socket: {0}")]
     Socket(#[from] Box<dyn error::Error>),
 
@@ -14,5 +15,19 @@ pub enum TcError {
     Netlink(String),
 
     #[error("Failed to decode netlink message: {0}")]
+    NetlinkDecode(String),
+}
+#[derive(Debug, Error)]
+pub enum TcError {
+    #[error("rust-netlink error: {0}")]
+    Netlink(#[from] NetlinkError),
+
+    #[error("Failed to decode field: {0}")]
     Decode(String),
+
+    #[error("Failed to unmarshal struct: {0}")]
+    UnmarshalStruct(#[from] Box<ErrorKind>),
+
+    #[error("Inavalid attribute: {0}")]
+    InvalidAttribute(String),
 }
