1ed2e397c4e657067c1e7170ba0d1117cd36c81c
mmynk <mohit.ritanil@gmail.com>
2023-11-14 18:41:23 +0000

    Add more inversion of control
    The library now depends on the caller to retrieve netlink messages and pass it to `netlink-tc`.
    `netlink-tc` accepts `Vec<NetlinkMessage<RtnlMessage>>` which can be fetched using `netlink-packet-route`.
    The message is then parsed and processed as earlier.

--- src/lib.rs
+++ src/lib.rs
@@ -9,59 +9,198 @@
 //! ```rust
 //! use netlink_tc as tc;
 //!
-//! // Get list of qdiscs
-//! let qdiscs = tc::qdiscs::<tc::Netlink>().unwrap();
+//! // Retrive netlink messages using `netlink-packet-route`.
+//! // See `examples` for more details.
+//! let messages: Vec<NetlinkMessage<RtnlMessage>>; // = ...
 //!
-//! // Get list of classes
-//! let classes = tc::classes::<tc::Netlink>().unwrap();
+//! // Get list of tc qdiscs or classes
+//! let qdiscs = tc::tc_stats(messages).unwrap();
 //!
-//! // Get class for given interface
-//! let class = tc::class::<tc::Netlink>("eth0").unwrap();
+//! // Get list of links
+//! let links = tc::links(messages).unwrap();
 //! ```
 
+pub use class::*;
+pub use errors::*;
+pub use qdiscs::*;
+pub use types::*;
+
+use netlink_packet_core::{NetlinkMessage, NetlinkPayload};
+use netlink_packet_route::{
+    link as netlink_link, tc as netlink_tc, LinkMessage as NlLinkMessage, RtnlMessage,
+    TcMessage as NlTcMessage,
+};
+use netlink_packet_utils::{nla::Nla, Emitable};
+
 pub mod errors;
-pub mod link;
-pub mod tc;
 pub mod types;
 
 mod class;
 mod constants;
-mod netlink;
+mod link;
 mod qdiscs;
+mod tc;
 
 #[cfg(test)]
 mod test_data;
 #[cfg(test)]
 mod tests;
 
-pub use class::*;
-pub use netlink::*;
-pub use qdiscs::*;
-pub use types::*;
-
-/// Get list of all `tc` qdiscs and classes.
-pub fn tc_stats<T: netlink::NetlinkConnection>() -> Result<Vec<Tc>, errors::TcError> {
-    tc::tc_stats::<T>()
+/// Possible message types for `tc` messages.
+/// A subset of `rtnl::RtnlMessage` enum.
+pub enum RtNetlinkMessage {
+    GetQdisc(TcMsg),  // RTM_GETQDISC
+    GetClass(TcMsg),  // RTM_GETCLASS
+    GetLink(LinkMsg), // RTM_GETLINK
 }
 
-/// Get list of all `tc` qdiscs.
-pub fn qdiscs<T: netlink::NetlinkConnection>() -> Result<Vec<Tc>, errors::TcError> {
-    tc::qdiscs::<T>()
+fn to_tc(tc_message: NlTcMessage) -> TcMsg {
+    let NlTcMessage {
+        header: tc_header,
+        nlas,
+        ..
+    } = tc_message;
+    let header = TcHeader {
+        index: tc_header.index,
+        handle: tc_header.handle,
+        parent: tc_header.parent,
+    };
+    let mut attrs = Vec::new();
+
+    for nla in nlas {
+        match nla {
+            netlink_tc::Nla::Kind(kind) => attrs.push(TcAttr::Kind(kind)),
+            netlink_tc::Nla::Options(tc_opts) => {
+                let mut opts = Vec::new();
+                for opt in tc_opts {
+                    match opt {
+                        netlink_tc::TcOpt::Ingress => {
+                            let option = TcOption {
+                                kind: 0u16, // TODO: what is Ingress kind?
+                                bytes: vec![],
+                            };
+                            opts.push(option);
+                        }
+                        netlink_tc::TcOpt::U32(nla) => {
+                            let mut buf = vec![0u8; nla.value_len()];
+                            nla.emit_value(buf.as_mut_slice());
+                            let option = TcOption {
+                                kind: nla.kind(),
+                                bytes: buf,
+                            };
+                            opts.push(option);
+                        }
+                        netlink_tc::TcOpt::Matchall(nla) => {
+                            let mut buf = vec![0u8; nla.value_len()];
+                            nla.emit_value(buf.as_mut_slice());
+                            let option = TcOption {
+                                kind: nla.kind(),
+                                bytes: buf,
+                            };
+                            opts.push(option);
+                        }
+                        netlink_tc::TcOpt::Other(nla) => {
+                            let mut buf = vec![0u8; nla.value_len()];
+                            nla.emit_value(buf.as_mut_slice());
+                            let option = TcOption {
+                                kind: nla.kind(),
+                                bytes: buf,
+                            };
+                            opts.push(option);
+                        }
+                        _ => (),
+                    };
+                }
+                attrs.push(TcAttr::Options(opts));
+            }
+            netlink_tc::Nla::Stats(tc_stats) => {
+                let mut buf = vec![0u8; tc_stats.buffer_len()];
+                tc_stats.emit(buf.as_mut_slice());
+                attrs.push(TcAttr::Stats(buf));
+            }
+            netlink_tc::Nla::Stats2(tc_stats) => {
+                let mut stats2 = Vec::new();
+                for stat in tc_stats {
+                    match stat {
+                        netlink_tc::Stats2::StatsBasic(bytes) => {
+                            stats2.push(TcStats2::StatsBasic(bytes))
+                        }
+                        netlink_tc::Stats2::StatsQueue(bytes) => {
+                            stats2.push(TcStats2::StatsQueue(bytes))
+                        }
+                        netlink_tc::Stats2::StatsApp(bytes) => {
+                            stats2.push(TcStats2::StatsApp(bytes))
+                        }
+                        _ => (),
+                    }
+                }
+                attrs.push(TcAttr::Stats2(stats2));
+            }
+            netlink_tc::Nla::XStats(bytes) => attrs.push(TcAttr::Xstats(bytes)),
+            netlink_tc::Nla::Rate(bytes) => attrs.push(TcAttr::Rate(bytes)),
+            netlink_tc::Nla::Fcnt(bytes) => attrs.push(TcAttr::Fcnt(bytes)),
+            netlink_tc::Nla::Stab(bytes) => attrs.push(TcAttr::Stab(bytes)),
+            netlink_tc::Nla::Chain(bytes) => attrs.push(TcAttr::Chain(bytes)),
+            netlink_tc::Nla::HwOffload(byte) => attrs.push(TcAttr::HwOffload(byte)),
+            _ => (),
+        }
+    }
+
+    TcMsg { header, attrs }
 }
 
-/// Get list of all `tc` classes.
-pub fn classes<T: netlink::NetlinkConnection>() -> Result<Vec<Tc>, errors::TcError> {
-    tc::classes::<T>()
+fn to_link(link_message: NlLinkMessage) -> LinkMsg {
+    let NlLinkMessage {
+        header: link_header,
+        nlas,
+        ..
+    } = link_message;
+    let header = LinkHeader {
+        index: link_header.index,
+    };
+
+    let mut name = String::new();
+    for nla in nlas {
+        if let netlink_link::nlas::Nla::IfName(if_name) = nla {
+            name = if_name;
+        }
+    }
+
+    LinkMsg {
+        header,
+        attr: LinkAttr { name },
+    }
 }
 
-/// Get list of all `tc` classes for a given interface.
-pub fn class<T: netlink::NetlinkConnection>(name: &str) -> Result<Vec<Tc>, errors::TcError> {
-    tc::class::<T>(name)
+fn parse(
+    messages: Vec<NetlinkMessage<RtnlMessage>>,
+) -> Result<Vec<RtNetlinkMessage>, NetlinkError> {
+    let mut tc_messages = Vec::new();
+    for message in messages {
+        match message.payload {
+            NetlinkPayload::InnerMessage(RtnlMessage::NewQueueDiscipline(message)) => {
+                tc_messages.push(RtNetlinkMessage::GetQdisc(to_tc(message.clone())))
+            }
+            NetlinkPayload::InnerMessage(RtnlMessage::NewTrafficClass(message)) => {
+                tc_messages.push(RtNetlinkMessage::GetClass(to_tc(message.clone())))
+            }
+            NetlinkPayload::InnerMessage(RtnlMessage::NewLink(message)) => {
+                tc_messages.push(RtNetlinkMessage::GetLink(to_link(message.clone())))
+            }
+            _ => (),
+        }
+    }
+    Ok(tc_messages)
 }
 
-/// Get list of all `tc` classes for a given interface index.
-pub fn class_for_index<T: netlink::NetlinkConnection>(
-    index: u32,
-) -> Result<Vec<Tc>, errors::TcError> {
-    tc::class_for_index::<T>(index)
+/// Parse `tc` queueing disciplines and classes for the corresponding Netlink messages.
+pub fn tc_stats(messages: Vec<NetlinkMessage<RtnlMessage>>) -> Result<Vec<Tc>, TcError> {
+    let messages = parse(messages).map_err(TcError::Netlink)?;
+    tc::tc_stats(messages)
+}
+
+/// Parse `link` messages for the corresponding Netlink messages
+pub fn links(messages: Vec<NetlinkMessage<RtnlMessage>>) -> Result<Vec<Link>, LinkError> {
+    let messages = parse(messages).map_err(LinkError::Netlink)?;
+    link::links(messages)
 }
